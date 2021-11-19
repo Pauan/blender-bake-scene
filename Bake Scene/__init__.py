770d2d22fc660b58233c3a990a45a0c0b5b8ca34
Pauan <pauanyu+github@pm.me>
2021-11-19 03:47:43 +0100

    Version 1.1

--- Bake Scene/__init__.py
+++ Bake Scene/__init__.py
@@ -20,7 +20,7 @@
 bl_info = {
     "name": "Bake Scene",
     "author": "Pauan",
-    "version": (1, 0),
+    "version": (1, 1),
     "blender": (2, 80, 0),
     "location": "Output Settings > Bake Scene",
     "description": "Bakes your entire scene into textures",
